05e241c06b87c58f751009bb8fb9e54584e5b7bd
Евгений Гилев <euggil@yandex.ru>
2021-11-29 16:23:22 +0500

    Добавьте файлы проекта.

---
+++ SF.Mod11.TelegramBotEvents/BotCredentials.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SF.Mod11.TelegramBotEvents
+{
+    public static class BotCredentials
+    {
+        public static readonly string BotToken = "2131549515:AAEzRSrLYGfcX9kGN1cVbH6chzf6tsf0YgA";
+    }
+}
---
+++ SF.Mod11.TelegramBotEvents/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Telegram.Bot;
+
+namespace SF.Mod11.TelegramBotEvents
+{
+    class Program
+    {
+        static ITelegramBotClient botClient;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var botClient = new TelegramBotClient(BotCredentials.BotToken);
+            var me = botClient.GetMeAsync().Result;
+            Console.WriteLine("Привет! Меня зовут {0}.", me.FirstName);
+
+
+        }
+    }
+}
